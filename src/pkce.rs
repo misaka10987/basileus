e5a22751c16c18b6981415dfd7827ed6577094a2
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-02-05 11:54:33 +0800

    add documentation for `pkce.rs`

--- src/pkce.rs
+++ src/pkce.rs
@@ -8,6 +8,7 @@ use crate::{
     err::{PkceAuthError, PkceTokenError},
 };
 
+/// A client PKCE code challenge, as defined in [RFC 7636](https://datatracker.ietf.org/doc/html/rfc7636#section-4.2).
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct CodeChallenge {
@@ -20,6 +21,7 @@ pub struct CodeChallenge {
 }
 
 impl CodeChallenge {
+    /// Create a new `CodeChallenge` object with specified base64URL-encoded code challenge.
     pub fn new(challenge: String) -> Self {
         Self {
             challenge,
@@ -27,6 +29,7 @@ impl CodeChallenge {
         }
     }
 
+    /// Verify the `code_verifier` by checking if the hash matches the stored `code_challenge`.
     pub fn verify(&self, code_verifier: &str) -> bool {
         let hash = Sha256::digest(code_verifier);
         let encoded = BASE64_URL_SAFE.encode(hash);
@@ -36,17 +39,22 @@ impl CodeChallenge {
 
 impl Display for CodeChallenge {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "PKCE {}:{}", self.method, self.challenge)
+        write!(f, "{}:{}", self.method, self.challenge)
     }
 }
 
+/// A pending PKCE authentication request.
 pub struct Pkce {
+    /// The authorized user name.
     pub user: String,
+    /// The associated code challenge from the PKCE authorization request.
     pub code_challenge: CodeChallenge,
+    /// Time of creation.
     pub begin: Instant,
 }
 
 impl Pkce {
+    /// Create a new `Pkce` object with specified authorized user and code challenge.
     pub fn new(user: String, code_challenge: CodeChallenge) -> Self {
         Self {
             user,
@@ -55,6 +63,8 @@ impl Pkce {
         }
     }
 
+    /// Check if the PKCE request is still valid.
+    /// Expiry time is set to 10 minutes (600 seconds).
     pub fn valid(&self) -> bool {
         self.begin.elapsed().as_secs() <= 600
     }
@@ -74,6 +84,9 @@ impl PkceModule {
 }
 
 impl Basileus {
+    /// Handle a PKCE authorization request.
+    ///
+    /// If the authorization is successful, returns a base64URL-encoded [authorization code](https://datatracker.ietf.org/doc/html/rfc6749#section-4.1.2).
     pub async fn pkce_auth_req(
         &self,
         user: &str,
@@ -99,6 +112,11 @@ impl Basileus {
         Ok(auth_code)
     }
 
+    /// Handle a PKCE access token request.
+    ///
+    /// A successful request requires a valid previously issued authorization code (through [`Self::pkce_auth_req`]) and a matching code verifier.
+    ///
+    /// Returns the token if successful.
     pub fn pkce_token_req(
         &self,
         code: &str,
